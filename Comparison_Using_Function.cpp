2f316e6ecad83888791c2ea17db11a2631b4113c
Vaibhav Sharma <43714751+Vaibhavabhaysharma@users.noreply.github.com>
2020-04-04 15:46:20 +0530

    Revert "Update Comparison_Using_Function.cpp"

--- Comparison_Using_Function.cpp
+++ Comparison_Using_Function.cpp
@@ -3,19 +3,29 @@ Input will contain four integers -a,b,c,d one in each line */
 
 #include <iostream>
 #include <cstdio>
-
 using namespace std;
+int gre;
 int max_of_four(int a, int b, int c, int d) {
-    int max=0;
-	if(a>max)
-	  max=a;
-	if(b>max) 
-	  max=b;
-    if(c>max)
-	  max=c;
-    if(d>max)
-	  max=d;
-    return max;
+    if(a>b) {
+        if(a>c) {
+            if(a>d) {
+                gre = a;
+            } else 
+            gre = d;
+        } else if (c>d) {
+            gre = c;
+        } else
+        gre = d;
+    } else if (b>c) {
+        if (b>d) {
+            gre = b;
+        } else 
+        gre = d;
+    } else if (c>d) {
+        gre = c;
+    } else 
+    gre = d;
+    return gre;
 }
 
 int main() {
@@ -25,7 +35,7 @@ int main() {
     cin >> c;
     cin >> d;
     int ans = max_of_four(a, b, c, d);
-     cout << "\n"<<ans;
+     cout << ans;
     
     return 0;
 }
